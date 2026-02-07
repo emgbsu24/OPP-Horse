f2152ad17a946a620059d53d19336b6386f5ed42
emma mcginley <emma.mcginley@bsu.edu>
2026-02-07 02:05:08 +0000

    I added race.h

---
+++ race.h
@@ -0,0 +1,15 @@
+#ifndef RACE_H
+#define RACE_H
+
+#include "horse.h"
+
+class Race {
+private:
+	static const int NUM_HORSES = 5;
+	static const int TRACK = 15;
+	Horse horses[NUM_HORSES];
+
+public:
+	Race();
+	void start();
+};
