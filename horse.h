89f579379a3e66beb2579a94ede5ec012cba46ec
emma mcginley <emma.mcginley@bsu.edu>
2026-02-07 01:52:28 +0000

    Added horse.h

---
+++ horse.h
@@ -0,0 +1,16 @@
+#ifndef HORSE_H
+#define HORSE_H
+
+class Horse {
+private:
+	int posistion;
+	int index;
+	int TRACK;
+
+public:
+	Horse();
+	void init(int index, int TRACK);
+	void advance();
+	void printLane() const;
+	bool isWinner() const;
+};
